fafb4c1bcf740bca65d8063c860f13f69d0f6646
fannyshehabi <fanny.shehabi@etudiant.univ-rennes.fr>
2023-10-20 08:22:48 +0200

    update service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicesService } from './services/services.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ServicesService],
 })
 export class AppModule {}
