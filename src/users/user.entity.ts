1eda3b9e4a754d021d40cb959931e440edbfd722
fannyshehabi <fanny.shehabi@etudiant.univ-rennes.fr>
2023-10-20 09:21:03 +0200

    premier controleur, premier endpoint et User Model

---
+++ src/users/user.entity.ts
@@ -0,0 +1,9 @@
+
+export class User {
+
+    id: number;
+
+    lastname: string;
+
+    firstname: string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { User } from './user.entity';
+
+const users : User[] = [
+    {
+        id: 0,
+        lastname: 'Doe',
+        firstname: 'John'
+    }
+]
+
+@Controller('users')
+export class UsersController {
+
+    @Get('all')
+    getAll(): string[] {
+        return ['a', 'b', 'c'];
+    }
+    
+}
