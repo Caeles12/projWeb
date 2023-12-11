9d91bf1cba968fe8f2d140ad8e676cfe05202304
Caeles12 <pol.jaouen@gmail.com>
2023-12-11 14:20:02 +0100

    Ajout Angular Material et tableau liste des utilisateurs
    Ajout d'un tableau mat-table pour afficher les informations des utilisateurs
    
    Co-Authored-By: feesh8 <133744318+feesh8@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UsersListComponent
-  ],
+  declarations: [AppComponent, UsersListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export class User {
+  constructor(
+    public id: number,
+    public password: string,
+    public lastname: string,
+    public firstname: string,
+    public age: number
+  ) {}
+}
+const users: User[] = [
+  new User(0, 'mdp1', 'Doe', 'John', 23),
+  new User(1, 'mdp2', 'Doe', 'Jane', 32),
+];
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.css'
+  styleUrl: './users-list.component.css',
 })
-export class UsersListComponent {
-
+export class UsersListComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'age'];
+  dataSource = users;
 }
