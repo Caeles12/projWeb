9d37e78e99ede0f91e6a24224b46d7a79e660ae7
Caeles12 <pol.jaouen@gmail.com>
2023-12-15 08:54:23 +0100

    Récupérer les données de l'utilisateur actuellement connecté

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiHelperService } from '../services/api-helper.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrl: './user-profile.component.css',
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  constructor(private api: ApiHelperService) {}
 
+  ngOnInit(): void {
+    this.api
+      .get({ endpoint: '/users/self' })
+      .then((response) => console.log(response));
+  }
 }
